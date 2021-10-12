feac7a8f6db7df5b8afe184cb4c0490b8f019f03
Ryan Scott <rscott@galois.com>
2021-10-12 16:44:39 -0400

    Support musl's implementation of read()
    All of the heavy lifting was done in
    https://github.com/GaloisInc/flexdis86/issues/35, which ensures that
    immediate arguments to `push` are sign-extended if necessary, and
    https://github.com/GaloisInc/macaw/issues/235, which adds corresponding
    sign-extension logic to `push`'s semantics in `macaw-x86`.
    
    Fixes #21.

---
+++ tests/binaries/read-musl.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+/**
+ * This is a variant of read.c which uses musl's implementation of read()
+ * rather than directly invoking syscall(SYS_read, ...).
+ * This serves as a regression test for issue #21.
+ *
+ * This test contains a buffer overrun resulting from a `read` syscall which
+ * causes a change in control flow that the verifier detects with the weird
+ * machine monitor
+ */
+
+// Use a struct to force the `trusted` flag to be placed in memory immediately
+// after the buffer
+typedef struct {
+  // Data buffer
+  char buf[4];
+  // Flag indicating whether the data in `buf` is trusted (system controlled)
+  // or untrusted (user controlled)
+  int trusted;
+} data;
+
+void unsafe_process(const data* d) {
+  // Process trusted data in a privileged way
+}
+
+void safe_process(const data* d) {
+  // Safely process untrusted data
+}
+
+void process_data(const data* d) {
+  if (d->trusted) {
+    unsafe_process(d);
+  } else {
+    safe_process(d);
+  }
+}
+
+int main(void) {
+  data d;
+  d.trusted = 0;
+
+  // Off-by-one read overflows into d.trusted
+  read(0, d.buf, 5);
+
+  process_data(&d);
+}
+
+void _start(void) {
+  main();
+}
