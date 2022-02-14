df3d7b926f1eebe2fdfc5985ef0e0e4697a0541f
Ryan Scott <rscott@galois.com>
2022-02-14 13:38:34 -0800

    CI: Use what4-solvers

--- .gitlab-ci/solvers.sh
+++ .gitlab-ci/solvers.sh
@@ -38,67 +38,12 @@ retry() {
     done
 }
 
-install_z3() {
-    is_exe "$BIN" "z3" && return
-
-    case "$RUNNER_OS" in
-        Linux) file="ubuntu-16.04.zip" ;;
-        macOS) file="osx-10.14.6.zip" ;;
-        Windows) file="win.zip" ;;
-    esac
-    curl -o z3.zip -sL "https://github.com/Z3Prover/z3/releases/download/z3-$Z3_VERSION/z3-$Z3_VERSION-x64-$file"
-
-    if $IS_WIN; then 7z x -bd z3.zip; else unzip z3.zip; fi
-    cp z3-*/bin/z3$EXT $BIN/z3$EXT
-    $IS_WIN || chmod +x $BIN/z3
-    rm z3.zip
-}
-
-install_cvc4() {
-    is_exe "$BIN" "cvc4" && return
-    version="${CVC4_VERSION#4.}" # 4.y.z -> y.z
-
-    case "$RUNNER_OS" in
-        Linux) file="x86_64-linux-opt" ;;
-        Windows) file="win64-opt.exe" ;;
-        macOS) file="macos-opt" ;;
-    esac
-    curl -o cvc4$EXT -sL "https://github.com/CVC4/CVC4/releases/download/$version/cvc4-$version-$file"
-
-    $IS_WIN || chmod +x cvc4$EXT
-    mv cvc4$EXT "$BIN/cvc4$EXT"
-}
-
-install_yices() {
-    is_exe "$BIN" "yices" && return
-    ext=".tar.gz"
-    case "$RUNNER_OS" in
-        Linux) file="pc-linux-gnu-static-gmp.tar.gz" ;;
-        macOS) file="apple-darwin18.7.0-static-gmp.tar.gz" ;;
-        Windows) file="pc-mingw32-static-gmp.zip" && ext=".zip" ;;
-    esac
-    curl -o "yices$ext" -sL "https://yices.csl.sri.com/releases/$YICES_VERSION/yices-$YICES_VERSION-x86_64-$file"
-
-    if $IS_WIN; then
-        7z x -bd "yices$ext"
-        mv "yices-$YICES_VERSION"/bin/*.exe "$BIN"
-    else
-        tar -xzf "yices$ext"
-        pushd "yices-$YICES_VERSION" || exit
-        sudo ./install-yices
-        popd || exit
-    fi
-    rm -rf "yices$ext" "yices-$YICES_VERSION"
-}
-
 install_system_deps() {
-    install_z3 &
-    install_cvc4 &
-    install_yices &
-    wait
-    export PATH=$PWD/$BIN:$PATH
-    echo "$PWD/$BIN" >> $GITHUB_PATH
-    is_exe "$BIN" z3 && is_exe "$BIN" yices
+  (cd $BIN && curl -o bins.zip -sL "https://github.com/GaloisInc/what4-solvers/releases/download/$SOLVER_PKG_VERSION/$BUILD_TARGET_OS-bin.zip" && unzip -o bins.zip && rm bins.zip)
+  chmod +x $BIN/*
+  cp $BIN/yices_smt2$EXT $BIN/yices-smt2$EXT
+  export PATH="$BIN:$PATH"
+  is_exe "$BIN" z3 && is_exe "$BIN" cvc4 && is_exe "$BIN" yices
 }
 
 
