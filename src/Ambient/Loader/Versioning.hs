733b91214303fd21931f28b95d2c3e9cc0ded58c
Ryan Scott <rscott@galois.com>
2022-04-21 13:07:31 -0400

    Remove awful takeSymbolVersionFileName hackery
    I originally put this hack into place because `symbol-versions-main.exe` had
    dynamic symbols that included leading paths
    (e.g., `symbol-versions/libsymbol-versions-bar.so`). But the fact that leading
    paths appear in the first place is because I was silly and compiled
    `symbol-versions-main.exe` with the wrong order of linker flags. Oops. In fact,
    `symbol-versions-main.exe` is broken due the leading paths appearing in the
    `DT_NEEDED` entries. You can't even invoke the binary because of this, even
    with `LD_LIBRARY_PATH=.` set!
    
    Once I compiled `symbol-versions-main.exe` with the proper flags, the leading
    paths no longer appear, thereby obviating the need for
    `takeSymbolVersionFileName` and friends. As such, I've deleted them. This makes
    the code nicer now and makes a subsequent commit easier to implement in the
    future.

--- src/Ambient/Loader/Versioning.hs
+++ src/Ambient/Loader/Versioning.hs
@@ -4,15 +4,10 @@ module Ambient.Loader.Versioning
   ( VersionedSymbol(..)
   , VersionedFunctionName
   , VersionedGlobalVarName
-  , eqVersionedSymbolStrict
-  , compareVersionedSymbolStrict
-  , takeSymbolVersionFileName
   ) where
 
 import qualified Data.ByteString.Char8 as BSC
-import           Data.Function ( on )
 import qualified Prettyprinter as PP
-import qualified System.FilePath as SF
 
 import qualified Data.BinarySymbols as BinSym
 import qualified What4.FunctionName as WF
@@ -25,21 +20,7 @@ import qualified What4.FunctionName as WF
 data VersionedSymbol sym = VerSym
   { versymSymbol :: !sym
   , versymVersion :: !BinSym.SymbolVersion
-  } deriving (Foldable, Functor, Traversable, Show)
-
--- | Check for equality modulo the leading directories in a
--- 'BinSym.VersionedSymbol's file path. For an explanation of why we check for
--- equality this way, see the Haddocks for 'takeSymbolVersionFileName'.
-instance Eq sym => Eq (VersionedSymbol sym) where
-  VerSym sym1 ver1 == VerSym sym2 ver2 =
-    (sym1 == sym2) && ((==) `on` takeSymbolVersionFileName) ver1 ver2
-
--- | Perform a comparison modulo the leading directories in a
--- 'BinSym.VersionedSymbol's file path. For an explanation of why we perform
--- comparisons this way, see the Haddocks for 'takeSymbolVersionFileName'.
-instance Ord sym => Ord (VersionedSymbol sym) where
-  compare (VerSym sym1 ver1) (VerSym sym2 ver2) =
-    compare sym1 sym2 <> (compare `on` takeSymbolVersionFileName) ver1 ver2
+  } deriving (Eq, Foldable, Functor, Ord, Traversable, Show)
 
 -- This code is cargo-culted from binary-symbols
 -- (https://github.com/GaloisInc/flexdis86/blob/9b899ed652c80dd22aa7dd9f6920d0d13a077bf6/binary-symbols/src/Data/BinarySymbols.hs#L67-L76),
@@ -61,39 +42,3 @@ type VersionedFunctionName = VersionedSymbol WF.FunctionName
 
 -- | A versioned 'BSC.ByteString' representing a global variable.
 type VersionedGlobalVarName = VersionedSymbol BSC.ByteString
-
--- | This behaves like the 'Eq' instance for 'VersionedSymbol', except that two
--- 'BinSym.VersionedSymbol's are considered distinct if they have different
--- leading directories in their file paths. (See the Haddocks for
--- 'takeSymbolVersionFileName' for more on this point.)
-eqVersionedSymbolStrict :: Eq sym => VersionedSymbol sym -> VersionedSymbol sym -> Bool
-eqVersionedSymbolStrict (VerSym sym1 ver1) (VerSym sym2 ver2) =
-  (sym1 == sym2) && (ver1 == ver2)
-infix 4 `eqVersionedSymbolStrict`
-
--- | This behaves like the 'Eq' instance for 'VersionedSymbol', except that two
--- 'BinSym.VersionedSymbol's are considered distinct if they have different
--- leading directories in their file paths. (See the Haddocks for
--- 'takeSymbolVersionFileName' for more on this point.)
-compareVersionedSymbolStrict :: Ord sym => VersionedSymbol sym -> VersionedSymbol sym -> Ordering
-compareVersionedSymbolStrict (VerSym sym1 ver1) (VerSym sym2 ver2) =
-  compare sym1 sym2 <> compare ver1 ver2
-
--- | File paths in 'BinSym.VersionedSymbol's can contain directories. For
--- example, a main binary might require a symbol located in @foo/bar.so@. This
--- can pose challenges when checking 'BinSym.SymbolVersion's for equality,
--- since @foo/bar.so@'s symbol versions will likely contain @bar.so@ rather
--- than @foo/bar.so@.
---
--- To ensure that these symbol versions match up, this function can be used to
--- take everything in a 'BinSym.VersionedSymbol's file path except for the file
--- name, which is all that really matters for linking/loading purposes. It's a
--- bit heavy-handed, but it gets the job done.
-takeSymbolVersionFileName :: BinSym.SymbolVersion -> BinSym.SymbolVersion
-takeSymbolVersionFileName sv =
-  case sv of
-    BinSym.UnversionedSymbol{}      -> sv
-    BinSym.ObjectDefaultSymbol{}    -> sv
-    BinSym.ObjectNonDefaultSymbol{} -> sv
-    BinSym.VersionedSymbol soName symName ->
-      BinSym.VersionedSymbol (BSC.pack (SF.takeFileName (BSC.unpack soName))) symName
