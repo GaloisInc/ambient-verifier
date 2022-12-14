1260a28676592adc8e541fe2b536bbeae9da71c4
Chris Hathhorn <chris.hathhorn@twosixtech.com>
2022-12-14 08:06:42 -0500

    Bump overrides-dsl submod, fix C override in demo.

--- demos/unbalanced-rop-read/overrides/function/getuid.c
+++ demos/unbalanced-rop-read/overrides/function/getuid.c
@@ -2,7 +2,7 @@
 
 // An override for getuid(2) that returns a symbolic value.  The override
 // returns the same value for each subsequent call.
-extern uid_t uid;
+uid_t uid;
 uid_t getuid() {
       return uid;
 }
